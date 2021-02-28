8796b5c996c4f8a902e6624c22f3128293a6aa73
Admin <admin@MacBook-Pro.local>
2021-02-28 21:16:16 +0300

    v1

---
+++ assets/scripts/anime.js
@@ -0,0 +1,28 @@
+const urlParams = new URLSearchParams(window.location.search);
+const animeId = urlParams.get('animeId');
+
+getAnimeDataFromServer()
+
+function getAnimeDataFromServer() {
+    axios.get(`https://api.jikan.moe/v3/anime/${animeId}`)
+        .then(response => {
+            console.log(response.data);
+            if (response.data) {
+                const h1 = document.querySelector('h1');
+                h1.innerText = response.data.title;
+
+                const trailerContainer = document.querySelector('.trailer-container');
+                trailerContainer.innerHTML = `
+                <iframe
+                        width="560" 
+                        height="315" 
+                        src="${response.data.trailer_url}" 
+                        frameborder="0"
+                         allowfullscreen></iframe> `
+
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        })
+}
